0a47050c3f642fb2c0f5650fc77e3a0ee8c8d94d
whatseven <1787609016@qq.com>
2018-06-24 20:13:50 +0800

    refact

---
+++ MyHtmlParser.py
@@ -0,0 +1,58 @@
+import re
+
+from bs4 import BeautifulSoup
+from ext import *
+
+class MyHTMLParser():
+    def __init__(self,vData):
+        self.__Soup=BeautifulSoup(vData)
+
+        self.__solveData()
+
+    def __solveData(self):
+        # Pronunciation
+        for FayinNode in self.__Soup.select("a[class='fayin']"):
+            FayinNode.img.decompose()
+            del FayinNode['href']
+        for FayinNode in self.__Soup.select("span[class='ei-g']"):
+            for Node in FayinNode.select("span[class='z']"):
+                Node.decompose()
+            for Node in FayinNode.select("a:nth-of-type(2)"):
+                Node.decompose()
+
+
+        # Example
+        for ExanpleNode in self.__Soup.select("span[class='x-g']"):
+            ExanpleNode.decompose()
+            del FayinNode['href']
+
+        # Totally
+        for PracpronNode in self.__Soup.select("span[class='pracpron']"):
+            PracpronNode.decompose()
+
+        # test=self.__Soup.prettify()
+        pass
+
+    def getData(self):
+        self.__Soup.head.append(self.__Soup.new_tag(
+            "meta",charset="UTF-8"
+        ))
+
+        # self.__Soup.head.append(self.__Soup.new_tag(
+        #     "script",type="text/javascript"
+        # ))
+        # self.__Soup.head.script.string=JQUERYSTATIC
+        #
+        # self.__Soup.head.append(self.__Soup.new_tag(
+        #     "script",type="text/javascript"
+        # ))
+        # self.__Soup.head.script.string=JSSTATIC
+        #
+        # self.__Soup.head.append(self.__Soup.new_tag(
+        #     "style",type="text/css"
+        # ))
+        # self.__Soup.head.script.string=CSSSTATIC
+
+        return self.__Soup.prettify()
+
+
--- Server.py
+++ Server.py
@@ -1,68 +1,19 @@
 import hashlib
 import json
+import os
 import random
 import re
 import sqlite3
 import time
-from html.parser import HTMLParser
 
 import requests
 from flask import Flask, request, Response, abort
 
 from MDXTools.mdict_query import IndexBuilder
-from ext import WORDRECORD
+from MyHtmlParser import MyHTMLParser
+from ext import WORDRECORD, CSSSTATIC, JSSTATIC, JQUERYSTATIC
 
 app=Flask(__name__)
-
-class MyHTMLParser(HTMLParser):
-    def __init__(self):
-        super(MyHTMLParser, self).__init__()
-        self.data = ''
-
-    def handle_starttag(self, tag, attrs):
-        """
-        recognize start tag, like <div>
-        :param tag:
-        :param attrs:
-        :return:
-        """
-        # print("Encountered a start tag:", tag)
-
-    def handle_endtag(self, tag):
-        """
-        recognize end tag, like </div>
-        :param tag:
-        :return:
-        """
-        # print("Encountered an end tag :", tag)
-
-    def handle_data(self, data):
-        """
-        recognize data, html content string
-        :param data:
-        :return:text
-        """
-        if '•' in data:
-            if not re.search(r"[a-zA-Z]", data):
-                if not len(data)<4:
-                    self.data += data + '\n'
-
-    def handle_startendtag(self, tag, attrs):
-        """
-        recognize tag that without endtag, like <img />
-        :param tag:
-        :param attrs:
-        :return:
-        """
-        # print("Encountered startendtag :", tag)
-
-    def handle_comment(self, data):
-        """
-
-        :param data:
-        :return:
-        """
-        # print("Encountered comment :", data)
 
 # Add word
 @app.route("/add",methods=["GET","POST"])
@@ -97,78 +48,71 @@ def Transaction():
     if 'GET'==request.method:
         pass
     elif 'POST'==request.method:
-        Word = request.form.get('word')
-        # Dict Parser
-        builder = IndexBuilder('MDXData/collins.mdx')
-        ResultWord = builder.mdx_lookup(Word)
-        ResultTransaction=""
-        if len(ResultWord) == 0:
-            # # YouDao
-            # appKey = '4fd37dd83c3b4a86'
-            # secretKey = '0euicKCl8dubyDXbvwbgYDMF3Rt6TWGf'
-            # q = Word
-            # fromLang = 'EN'
-            # toLang = 'zh-CHS'
-            # salt = random.randint(1, 65536)
-            # sign = appKey + q + str(salt) + secretKey
-            # m1 = hashlib.md5()
-            # m1.update(sign.encode())
-            # sign = m1.hexdigest()
-            # r = requests.post("http://openapi.youdao.com/api"
-            #                   , data={"appKey": appKey
-            #         , "q": q
-            #         , "from": fromLang
-            #         , "to": toLang
-            #         , "salt": salt
-            #         , "sign": sign})
-            # result = json.loads(r.text)
-            # ResultTransaction = "From YouDao\n" + result["translation"][0]
+        Word=request.form.get("word")
+        # # YouDao
+        # appKey = '4fd37dd83c3b4a86'
+        # secretKey = '0euicKCl8dubyDXbvwbgYDMF3Rt6TWGf'
+        # q = Word
+        # fromLang = 'EN'
+        # toLang = 'zh-CHS'
+        # salt = random.randint(1, 65536)
+        # sign = appKey + q + str(salt) + secretKey
+        # m1 = hashlib.md5()
+        # m1.update(sign.encode())
+        # sign = m1.hexdigest()
+        # r = requests.post("http://openapi.youdao.com/api"
+        #                   , data={"appKey": appKey
+        #         , "q": q
+        #         , "from": fromLang
+        #         , "to": toLang
+        #         , "salt": salt
+        #         , "sign": sign})
+        # result = json.loads(r.text)
+        # ResultTransaction = "From YouDao\n" + result["translation"][0]
 
-            # Baidu
-            appKey = '20180418000147886'
-            secretKey = '8tuKIpNnEoAOzrr8mrPn'
-            q = Word
-            fromLang = 'en'
-            toLang = 'zh'
-            salt = random.randint(32768, 65536)
-            sign = appKey + q + str(salt) + secretKey
-            m1 = hashlib.md5()
-            m1.update(sign.encode())
-            sign = m1.hexdigest()
-            r = requests.post("http://api.fanyi.baidu.com/api/trans/vip/translate"
-                              , data={"appid": appKey
-                    , "q": q
-                    , "from": fromLang
-                    , "to": toLang
-                    , "salt": salt
-                    , "sign": sign})
-            result = json.loads(r.text)
-            ResultTransaction = "From BaiDu\n" + result["trans_result"][0]['dst']
-        else:
-            parser = MyHTMLParser()
-            parser.feed(ResultWord[0])
-            ResultTransaction = parser.data
+        # Baidu
+        appKey = '20180418000147886'
+        secretKey = '8tuKIpNnEoAOzrr8mrPn'
+        q = Word
+        fromLang = 'en'
+        toLang = 'zh'
+        salt = random.randint(32768, 65536)
+        sign = appKey + q + str(salt) + secretKey
+        m1 = hashlib.md5()
+        m1.update(sign.encode())
+        sign = m1.hexdigest()
+        r = requests.post("http://api.fanyi.baidu.com/api/trans/vip/translate"
+                          , data={"appid": appKey
+                , "q": q
+                , "from": fromLang
+                , "to": toLang
+                , "salt": salt
+                , "sign": sign})
+        result = json.loads(r.text)
+        ResultTransaction = "From BaiDu\n" + result["trans_result"][0]['dst']
 
         return json.dumps(ResultTransaction)
 
-# Download Database
-@app.route("/download",methods=["GET"])
-def Download():
+# Sychronize
+@app.route("/sychronize",methods=["GET","POST"])
+def Sychronize():
     if 'GET'==request.method:
-        with open(WORDRECORD, 'rb') as TargetFile:
-            data = TargetFile.read()
-        response = Response(data, content_type='application/octet-stream')
-        return response
-    elif 'POST'==request.method:
-        pass
-
-# Upload
-@app.route("/upload",methods=["POST"])
-def Upload():
-    if 'POST'==request.method:
-        try:
+        LocalTime=request.values.get("LocalTime")
+        # Download
+        if os.stat(WORDRECORD).st_mtime>=float(LocalTime):
             with open(WORDRECORD, 'rb') as TargetFile:
                 data = TargetFile.read()
+            response = Response(data, content_type='application/octet-stream')
+            response.status_code=200
+            return response
+        else:
+            response = Response()
+            response.status_code=302
+            return response
+    elif 'POST'==request.method:
+        try:
+            with open(WORDRECORD, 'wb') as TargetFile:
+                TargetFile.write(request.data)
             response = Response()
             return response
         except Exception as e:
